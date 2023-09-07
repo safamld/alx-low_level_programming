05ab7f2857471c1529deafa3ea13e0cf9339d7c2
safa <safaaamd7@gmail.com>
2023-09-07 16:40:58 -0700

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
 */
 int main(void)
 {
-Printf("size of char : %zu byte(s)\n", sizeof(char));
-Printf("size of int : %zu byte(s)\n", sizeof(int));
-Printf("size of float : %zu byte(s)\n", sizeof(float));
-Printf("size of long int : %zu byte(s)\n", sizeof(long int));
-Printf("size of long long int : %zu byte(s)\n", sizeof(long long int));
+printf("Size of char : %lu byte(s)\n", sizeof(char));
+printf("Size of int : %lu byte(s)\n", sizeof(int));
+printf("Size of float : %lu byte(s)\n", sizeof(float));
+printf("Size of long int : %lu byte(s)\n", sizeof(long int));
+printf("Size of long long int : %lu byte(s)\n", sizeof(long long int));
 return (0);
 }
