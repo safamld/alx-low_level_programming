7d2cccfa870ad77ea24ad9516e74e1486497324a
safa <safaaamd7@gmail.com>
2023-09-07 12:20:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-/*  print message without using printf that returns  */
+/*  print message
+ *  rogramming is like building a multilingual puzzle
+ *   without using printf that returns  */
 int main(){
-puts("Programming is like building a multilingual puzzle\n");
+puts('Programming is like building a multilingual puzzle\n');
 return(0);
 }
