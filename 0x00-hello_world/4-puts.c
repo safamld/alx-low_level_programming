d60da8391c15a3ba3bb9069c4e17dd89866b8110
safa <safaaamd7@gmail.com>
2023-09-07 12:24:15 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
  *  rogramming is like building a multilingual puzzle
  *   without using printf that returns  */
 int main(){
-puts('Programming is like building a multilingual puzzle\n');
+puts("Programming is like building a multilingual puzzle\n");
 return(0);
 }
