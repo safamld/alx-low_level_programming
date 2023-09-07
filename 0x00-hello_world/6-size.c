b6df4a7f275023dd38a9ffa732810d911342df6f
safa <safaaamd7@gmail.com>
2023-09-07 16:21:47 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main- print the size of type
+*Return:sucess
+*/
+int main(void)
+{
+Printf("size of char : %zu byte(s)\n", sizeof(char));
+Printf("size of int : %zu byte(s)\n", sizeof(int));
+Printf("size of float : %zu byte(s)\n", sizeof(float));
+Printf("size of long int : %zu byte(s)\n", sizeof(long int));
+Printf("size of long long int : %zu byte(s)\n", sizeof(long long int));
+return (0);
+}
