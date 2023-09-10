b69fe6e255057647fe6cc22b24513561725b4c47
safa <safaaamd7@gmail.com>
2023-09-10 12:22:25 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+/**
+* main - Entry point
+*
+* Description: Prints the lowercase alphabet using the putchar function.
+*     
+* Return: Always 0 (Success).
+*/
+int main(void)
+{	    
+char letter = 'a';
+
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+
+putchar('\n');
+
+return (0);
+}
+
