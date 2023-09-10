c4b7e5288d0598e01346a1e10a284754f97e9948
safa <safaaamd7@gmail.com>
2023-09-10 08:03:01 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,29 +1,28 @@
-#include	<stdlib.h> 
-#include	<time.h>
-#include	<stdio.h> 
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * main - fonction de lzst degit 
- * 
- * *Return:0 
- */ 
-int main(void) 
+* main - fonction de lzst degit 
+* Return:0 
+*/ 
+int  main(void) 
 { 
 int n;    
 srand(time(0)); 
 n = rand() - RAND_MAX / 2; 
 /* your code goes there */
 int last_digit	=	n	%	10;
-if (last_digit>5)
+if (last_digit	>	5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, las_digit);
 }
-else if (last_digit=0)
+else if (last_digit	==	0)
 {
-printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-else if (last_digit < 5 && last_digit !=0  )
+else if (last_digit < 6  && last_digit !=	0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 return (0); 
 }
