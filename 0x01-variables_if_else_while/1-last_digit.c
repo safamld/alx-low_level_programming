e69ed422b5a5a45ae1aa6f32427e45cba0a74140
safa <safaaamd7@gmail.com>
2023-09-10 08:33:46 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ n = rand() - RAND_MAX / 2;
 /*your code goes there */
 if (last_digit	>	5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, las_digit);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 }
 else if (last_digit	==	0)
 {
