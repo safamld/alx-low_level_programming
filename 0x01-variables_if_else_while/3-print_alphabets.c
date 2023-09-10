5e06eefb379a7215ea0c8e124100313cfbebe6ed
safa <safaaamd7@gmail.com>
2023-09-10 16:45:10 -0700

    alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-/**
+/* 
+* main - fonction print alphabets
+*
+* description:pront alphabetic upercase and lowercase
+*
+* Return: sucess
 */
 int main(void)
 {
 char i, j;
-for (i= 'a';i<='z';i++)
+for (i = 'a'; i < = 'z'; i++)
 {
 puchar(i\n);
 }
-for (j= 'A';j<='Z';j++)
+for (j = 'A';j < = 'Z'; j++)
 {
 puchar(j\n);
 }
