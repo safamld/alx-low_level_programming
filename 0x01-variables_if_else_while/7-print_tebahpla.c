87c7f145071d072c83a2a1e4e0cb9ed08891a15f
safa <safaaamd7@gmail.com>
2023-09-11 07:16:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+* main - function reverse
+*
+* description: print alphabet dune maniere inverse
+*
+* Return: sucess
+*\
+int main(void)
+{
+char lowercase;
+lowercase = 'z';
+while (lowercase < 'a')
+{
+putchar(lowercase);
+lowercase--;
+}
+putchar('\n');
+return (0);
+}
