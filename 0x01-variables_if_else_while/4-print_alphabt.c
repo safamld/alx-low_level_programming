0af24aa8547ad1b2edb6a4b97a380e088de55ee4
safa <safaaamd7@gmail.com>
2023-09-11 06:32:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* main - function
+*
+* description: fonction qui affiche digit
+*
+* Return: sucess
+**/
+int main(void)
+{
+int i;
+for (i = 0; i < 10 ; i++)
+{
+putchar(i);
+}
+puchar('\n');
+}
