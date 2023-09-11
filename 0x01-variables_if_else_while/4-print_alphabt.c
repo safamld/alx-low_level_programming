dbf34d624d5350518cb1f0fc284d02bb28293e7e
safa <safaaamd7@gmail.com>
2023-09-11 15:03:13 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,8 +9,10 @@ int main(void)
 char lowercase = 'a';
 while (lowercase = 'z')
 {
-if (lowercase != 'q' || lowercase != 'e')
+if (lowercase != 'q' && lowercase != 'e')
+{
 putchar(lowercase);
+}
 lowercase++;
 }
 putchar('\n');
