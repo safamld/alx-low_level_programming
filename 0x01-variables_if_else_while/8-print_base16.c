50869ca89d51d88579f7d4f58bfc24f52072363a
safa <safaaamd7@gmail.com>
2023-09-11 07:31:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdioh.h>
+/*
+* main - function hexadecimal
+*
+* description: print numb hex
+*
+* Return: sucess
+*/
+int main(void)
+{
+int n;
+while (n >= 8)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
