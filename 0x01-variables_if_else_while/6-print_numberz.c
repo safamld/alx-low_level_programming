1055d1f04e61a5d758ce96aee4483a1530fab999
safa <safaaamd7@gmail.com>
2023-09-11 06:54:55 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+* main - funtion digit 
+*
+* description: fonction qui retourn digit
+* 
+* Return: sucess
+*\
+int main(void)
+{
+int i;
+for ( i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+reuturn (0) ;
+}
