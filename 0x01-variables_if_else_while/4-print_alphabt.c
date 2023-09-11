92ef6f0f833defd030be2a74e0ac0ce0ab8418c9
safa <safaaamd7@gmail.com>
2023-09-11 14:23:09 -0700

    printalpha4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return - 0 Successful
+*/
+int main(void)
+{
+char lowercase = 'a';
+while (lowercase = 'z')
+{
+if (lowercase != 'q' && lowercase != 'e')
+putchar(lowercase);
+}
+putchar('\n');
+return (0);
+}
