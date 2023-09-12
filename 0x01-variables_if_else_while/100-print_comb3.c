3c9a407b498cca09f472c21dbf1bd7465389ae95
safa <safaaamd7@gmail.com>
2023-09-11 17:43:15 -0700

    gffg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@ int main(void)
 {
 int i = 0;
 int j =1 ;
-while (i < 10)
+while (i < 8)
 {
-while(j < 10)
+while(j < 9)
 {
 putchar(i + '0');
 putchar(i + '0'); 
-if (i != 8 || j != 9)
+if (!(i == 8 && j == 9))
 {
 putchar(',');
 putchar(' ');
