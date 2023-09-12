e7b5e814cc0fa620c0e6107575dbef5d98f2e891
safa <safaaamd7@gmail.com>
2023-09-12 13:13:39 -0700

    gfh

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 {
 int i = 0;
 char alph = 'a';
-while (i <= 0)
+while (i <= 10)
 {
 while (alph <= 'z')
 {
@@ -16,7 +16,7 @@ _putchar(alph);
 alph++;
 }
 _putchar('\n');
-alph ='a';
+alph = 'a';
 i++;
 }
 }
