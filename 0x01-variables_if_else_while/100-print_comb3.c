f8eb795ffcd271494d54621c772e90cd9505a15d
safa <safaaamd7@gmail.com>
2023-09-11 17:01:15 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return - 0 Successful
+*/
+int main(void)
+{
+int i = 0;
+int j =1 ;
+while (i < 10)
+{
+while(j < 10)
+{
+if ( i != j)
+{
+putchar(("%d%d", i, j);
+j++;
+}}
+i++;
+putchart(',');
+putchart(' ');
+}
+putchart('\n');
+return (0)
+}
