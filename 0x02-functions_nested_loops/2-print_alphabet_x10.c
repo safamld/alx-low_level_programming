959d07109c6dc0560d9ed90f764176a0ca9ba77a
safa <safaaamd7@gmail.com>
2023-09-12 12:47:10 -0700

    printalpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /*
-* main - function alphabet
+* main - function alphabet.
 *
-* Return : Sucess 
+* Return : Always 0. 
 */
 void print_alphabet_x10(void)
 {
 int i = 0;
 char alph = 'a';
-while (i <= 10)
-{
 while (alph <= 'z')
 {
+while (i <= 10)
+{
 _putchar(alph);
-alph++;
-}
 i++;
+}
+alph++;
 }
 _putchar('\n');
 }
