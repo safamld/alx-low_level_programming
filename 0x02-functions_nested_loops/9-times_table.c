2968133b30967b7e44186427f069449b0e44f8be
safa <safaaamd7@gmail.com>
2023-09-13 09:00:51 -0700

    time9table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,27 +9,27 @@ void times_table(void)
 int a, b, c, d, e;
 for (a = 0; a < 10; a++)
 {
-	for (b = 0; b < 10; b++)
-	       {
-	       c = a * b;
-		if (c > 9)
-		{			
-		d = c % 10;
-		e = (c - d) / 10;
-		_putchar(44);
-		_putchar(32);
-		_putchar(e + '0');
-		_putchar(d + '0');
-		}
-		else
-		{
-		if (b != 0)
-		_putchar(44);
-		_putchar(32);
-		_putchar(32);
-		}
-		_putchar(c + '0');
-		}
+for (b = 0; b < 10; b++)
+{
+c = a * b;
+if (c > 9)
+{			
+d = c % 10;
+e = (c - d) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(e + '0');
+_putchar(d + '0');
+}
+else
+{
+if (b != 0)
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(c + '0');
+}
 }
 _putchar('\n');
 }
