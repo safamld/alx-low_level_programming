9811a57df677c3852fdbeccf2d212141f37857e0
safa <safaaamd7@gmail.com>
2023-09-13 14:57:31 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Function list natural number
+*
+* Description: Function print some of multiples of 3 and 5
+*
+* Return: Always sucess
+*/
+int main(void)
+{
+int i,s = 0;
+while (i < 1024)
+{
+if (i % 3 == 0) || (i % 5 == 0)
+{
+s += i;
+}
+i++;
+}
+return (0);
+}
