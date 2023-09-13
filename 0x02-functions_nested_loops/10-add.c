3a210d995d5b26303942b67ba2cd4811dbdb7a51
safa <safaaamd7@gmail.com>
2023-09-13 10:09:58 -0700

    addn

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - function adds two integers
+*
+* @a: Premier entier
+* @b: Deuxieme entier
+*
+* Return: Integer value
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
