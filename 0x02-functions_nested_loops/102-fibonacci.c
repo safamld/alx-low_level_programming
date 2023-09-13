905cb7fc560086f677229164f79b0b19dc524e12
safa <safaaamd7@gmail.com>
2023-09-13 16:06:22 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - Function billoc
+*
+* Descriotion:Program print first 50 fibonaci
+*
+*Return: Always sucess
+*/
+int main(void)
+{
+int i = 0;
+long  a = 1, b = 2;
+while (i < 50)
+{
+if (i == 0)
+printf("%ld", a);
+else if (i == 2)
+printf("%ld", b);
+else
+{
+b += a;
+a = b - a;
+printf(", %ld", b);
+}
+++i;
+}
+printf("\n");
+return (0);
+}
