622fdaaace1994878822f2885b0ba3b0aba124b0
safa <safaaamd7@gmail.com>
2023-09-13 13:59:11 -0700

    100 times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* print_times_table - Function print n
+*
+* @n: Number
+*
+* Return: Empty output
+*/
+void print_times_table(int n)
+{
+if (n >= 0 && n <= 15)
+{
+int num, mult, prod;
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 99)
+{
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+}
+}
+}
+}
