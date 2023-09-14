658b269686058079409340f38125cb4b74010ca4
safa <safaaamd7@gmail.com>
2023-09-13 17:19:20 -0700

    fib

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Function billoc
+*
+* Descriotion:Program print first ibonaci
+*
+*Return: Always sucess
+*/
+int main(void)
+{
+int i = 0;
+long j = 1, k = 2, sum = k;
+while (k + j < 4000000)
+{
+k += j;
+if (k % 2 == 0)
+sum += k;
+j = k - j;
+++i;
+}
+printf("%ld\n", sum);
+return (0);
+}
