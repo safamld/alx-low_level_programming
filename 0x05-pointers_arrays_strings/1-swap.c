658d6fad2a018afe35938ef665a6b4798e4feb7e
safa <safaaamd7@gmail.com>
2023-09-18 11:41:00 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* swap_int - function yhat swap numbers
+*
+* @a: parametre function
+* @b:parametre function
+*
+* Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
