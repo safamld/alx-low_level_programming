83bd95df1a2e39f4e426c797f9a69b80446f72b8
safa <safaaamd7@gmail.com>
2023-09-18 14:22:29 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_rev - function reverse
+*
+* @s: the string to reverse
+*
+* Return: nothing
+*/
+void print_rev(char *s);
+{
+int i;
+for (str[i] = '\0'; i = 0; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
