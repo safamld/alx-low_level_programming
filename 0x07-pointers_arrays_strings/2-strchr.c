c9b30d6c5d1477cae961344dc9b744ea347b6b60
safa <safaaamd7@gmail.com>
2023-09-25 14:49:11 -0700

    ./2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strchr - locates caracter in string
+* @c: the caracter 
+* @s: the string
+* Return: Null
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (s[i] == c)
+return (s + i);
+}
+return (NULL);
+}
