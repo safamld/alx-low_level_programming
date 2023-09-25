60bfdfcddc5dc400f5b824f27575c2648a06ac35
safa <safaaamd7@gmail.com>
2023-09-25 16:52:00 -0700

    ./3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* _strspn - function gets the lenght of prefix
+* @s: segment
+* @accept: input
+* Rethrn: sucess
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int v = 0;
+int i;
+while (*s)
+{
+
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+v++;
+break;
+}
+else if (accept[i + 1] == '\0')
+return (v);
+}
+s++;
+}
+return (v);
+}
