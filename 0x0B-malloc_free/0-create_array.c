64d0d9c6062eb096482d6d3294f6dddc744a4aa0
safa <safaaamd7@gmail.com>
2023-10-05 17:55:45 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ if (size == 0)
 return (NULL);
 s = malloc(size * sizeof(char));
 if (s == NULL)
-return(NULL);
+return (NULL);
 for (i = 0; i < size; i++)
 {
 s[i] = c;
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,1 +1,3 @@
-0x0B. C - malloc, free
+[master 2b7c937] readme file
+ 1 file changed, 1 insertion(+)
+ create mode 100644 0x0B-malloc_free/README.md
