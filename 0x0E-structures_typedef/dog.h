5fb3dfe6e6f188065b558a0a41c23e4afaf92cfd
safa <safaaamd7@gmail.com>
2023-10-10 05:37:17 -0700

    fix struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
-#include "main.h"
+#ifndef _HEADER_
+#define _HEADER_
 /**
-* struct dog - structure qui retourne type de chien 
+* struct dog - structure qui retourne type de chien
 * @name: the name of the dog
 * @age: the age
 * @owner: the owner
@@ -12,4 +13,12 @@ char *name;
 float age;
 char *owner;
 };
-
+/**
+* dog_t - typedef for dog structure
+*/
+typedef struct dog  dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
