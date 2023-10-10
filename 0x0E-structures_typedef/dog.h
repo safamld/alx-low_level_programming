2122f0d5566467e14780d46668f1f3c84cb2df16
safa <safaaamd7@gmail.com>
2023-10-10 04:28:55 -0700

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* struct dog - structure qui retourne type de chien 
+* @name: the name of the dog
+* @age: the age
+* @owner: the owner
+* return: sucess
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
