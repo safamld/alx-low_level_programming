242e7a10f7592e6ce8c7f1978321ab351e57cea4
safa <safaaamd7@gmail.com>
2023-10-11 16:13:24 -0700

    hsh

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_dog - print ia struct fog
+* @d: a dog structure
+* Return: sucess
+*/
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+printf("Age: %f\n", (d->age) ? d->age : 0);
+printf("Owner: %s\n", (d->owner) ? d->owner: "(nil)");
+}
+}
