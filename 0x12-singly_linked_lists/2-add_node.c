e096e6d0b2db0af26756c71f2d35bae274d4d86e
safa <safaaamd7@gmail.com>
2023-10-20 15:24:25 -0700

    function that prints all elements of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* add_node - function that adds a new node at the begining of a list
+* @h: pointer to the list
+* Return: the adress of the new element
+*/
+list_t *add_node(list_t **head, const char *str);
+{
+
