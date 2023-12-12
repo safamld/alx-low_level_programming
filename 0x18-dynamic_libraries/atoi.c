8700f40bd8f147db7b20d9cf67cb0ceb752c11dc
safa <safaaamd7@gmail.com>
2023-12-12 07:19:53 -0800

    task 0  dynamic libraries

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -1,17 +1,23 @@
 #include "main.h"
-int _atoi(char *s) {
+/**
+* _atoi- covert characters to integer
+* @s: The character to print
+*
+* Return: On success 1.
+*/
+int _atoi(char *s)
+{
 int result = 0;
 int sign = 1;
-// Check for sign
-if (*s == '-') {
+if (*s == '-')
+{
 sign = -1;
 s++;
 }
-// Convert characters to integer
-while (*s >= '0' && *s <= '9') {
+while (*s >= '0' && *s <= '9')
+{
 result = result * 10 + (*s - '0');
 s++;
 }
-// Apply sign
-return result * sign;
+return (result *sign);
 }
