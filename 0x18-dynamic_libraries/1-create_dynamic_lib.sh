4a6756a9e492505ed26f93d926420a5e3f5b8e17
safa <safaaamd7@gmail.com>
2023-12-12 08:10:25 -0800

    script that creates a dynamic library called liball.so from all the .c files that are in the current directory.

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextre -Werror -pedantic *.c -c -fPIC
+gcc -Wall -Wextra -Werror -pedantic *.c -c -fPIC
 gcc *.o -shared -o liball.so
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
--- 0x18-dynamic_libraries/strpbrk.c
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -1,9 +1,16 @@
 #include "main.h"
-char *_strpbrk(char *s, char *accept) {
-while (*s != '\0') {
-if (strchr(accept, *s) != NULL) {
-return s; // Return the pointer to the first occurrence of any character from accept
+char *_strpbrk(char *s, char *accept)
+{
+while (*s != '\0')
+{
+char *a = accept;
+while (*a != '\0') {
+if (*s == *a) {
+return s;  // Return a pointer to the first matching character
+}
+a++;
 }
 s++;
 }
+return NULL;  // Return NULL if no match is found
 }
