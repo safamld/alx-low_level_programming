a279e79598bdb6867c9f12733f3e0b7abd4f9a11
safa <safaaamd7@gmail.com>
2024-03-24 13:47:26 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,23 +2,24 @@
 /**
 * hash_table_t - check the code for
 * @size: size of the array
+*
 * Return: pointer to the newly hash table created.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_table;
-unsigned long int i;
-hash_table = malloc(sizeof(hash_table_t));
-if(hash_table == NULL)
-return (NULL);
-hash_table->size = size;
-hash_table->array = malloc(size * sizeof(hash_node_t *));
-if(hash_table->array == NULL)
-{
-free(hash_table);
-return(NULL);
-}
-for(i = 0; i < size; i++)
-hash_table->array[i] = NULL;
-return(hash_table);
+	hash_table_t *hash_table;
+	unsigned long int i;
+	hash_table = malloc(sizeof(hash_table_t));
+	if(hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if(hash_table->array == NULL)
+	{
+		free(hash_table);
+		return(NULL);
+	}
+	for(i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return(hash_table);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,11 @@
 #include "hash_tables.h"
+/**
+* key_index - gives the index of a key
+* @key: key to get index for
+* @size: size of the hash table
+*
+* Return: index fot the key
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 return (hash_djb2(key) % size);
