e9f0c048370956a5eaf3f5e16efba1bf1f08632f
safa <safaaamd7@gmail.com>
2024-04-09 07:59:33 -0700

    hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	hash_node_t **array;
 	unsigned long int i;
 	hash_table = malloc(sizeof(hash_table_t));
 	if(hash_table == NULL)
 		return (NULL);
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+	hash_table->array = array;
 	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if(hash_table->array == NULL)
-	{
-		free(hash_table);
-		return(NULL);
-	}
-	for(i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return(hash_table);
+	return (table);
 }
